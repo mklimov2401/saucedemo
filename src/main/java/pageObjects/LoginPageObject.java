ab794ae22838cf8aeee074a7bacaa06684f42e57
medved <mklimov2401@gmail.com>
2022-02-08 21:27:59 +0300

    Добавлен тест логин заблокированным пользователем.

--- src/main/java/pageObjects/LoginPageObject.java
+++ src/main/java/pageObjects/LoginPageObject.java
@@ -10,4 +10,5 @@ public class LoginPageObject {
     public final SelenideElement userName = Selenide.$(By.xpath(".//input[@id='user-name']"));
     public final SelenideElement password = Selenide.$(By.xpath(".//input[@id='password']"));
     public final SelenideElement loginButton = Selenide.$(By.xpath(".//input[@id='login-button']"));
+    public final SelenideElement error = Selenide.$(".error-message-container");
 }
---
+++ src/test/java/ru/klimov/locked/LoginLocked.java
@@ -0,0 +1,25 @@
+package ru.klimov.locked;
+
+import org.junit.jupiter.api.Test;
+import pageObjects.LoginPageObject;
+import ru.klimov.BaseTest;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.page;
+
+public class LoginLocked extends BaseTest {
+
+
+    @Test
+    void loginInLockedUserTest() {
+        LoginPageObject login = page(LoginPageObject.class);
+
+        open(config.hostname());
+
+        login.userName.setValue(config.lockedOutUser());
+        login.password.setValue(config.password());
+        login.loginButton.click();
+        login.error.shouldBe(text("Epic sadface: Sorry, this user has been locked out."));
+    }
+}
